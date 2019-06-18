83dcee0a3328b4915c71bb440907962a8fd839af
emy garcia <emygarcia1920@gmail.com>
2019-06-18 06:05:12 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(){
+  let array = [];
+  for( let i = 0; i < 25; i++){
+    return "I am " + i + " strange loop.";
+    
+    if( i > 25 ){
+      return `I am ${i} strange loops.";
+    }
+  }
+  return array; 
+}
